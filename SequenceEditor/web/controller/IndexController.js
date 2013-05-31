dd9c6a58c95c39ccd10605d06d2cd85087d5035c
Kevin LeShane <leshane@bu.edu>
2013-05-31 16:36:14 -0400

    Add and delete button functionality

--- SequenceEditor/web/controller/IndexController.js
+++ SequenceEditor/web/controller/IndexController.js
@@ -4,9 +4,14 @@
  */
 $(document).ready(function() { //don't run javascript until page is loaded
     //**********************************************/
+    var i = 0;
     $('#button').click(function(){
-        alert("I got clicked");
-        $(this).remove();
+        $("#bPAR").append("<button id="+ i++ +" style='font-weight:bold' class='btn btn-success'>"+ i +"</button>");
+    });
+    
+    $('#reset').click(function(){
+        $("button").remove(".btn-success");
+        i=0;
     });
 //**************************************************/
 });
