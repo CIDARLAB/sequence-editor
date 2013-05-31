9808d3f2935deab773d66f4c98769780fccdd683
Jenhan Tao <jenhantao@gmail.com>
2013-05-31 13:10:23 -0400

    initial commit of code

---
+++ SequenceEditor/web/controller/IndexController.js
@@ -0,0 +1,13 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+$(document).ready(function() { //don't run javascript until page is loaded
+    //**********************************************/
+    $('#button').click(function(){
+        alert("I got clicked");
+        $(this).remove();
+    });
+//**************************************************/
+});
+
