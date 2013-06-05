5e793d79b93869a6eb56529254992ed9150b9448
Jenhan Tao <jenhantao@gmail.com>
2013-06-05 11:47:13 -0400

    dragging and resizing work

--- SequenceEditor/web/controller/jenhanSandboxController.js
+++ SequenceEditor/web/controller/jenhanSandboxController.js
@@ -1,6 +1,22 @@
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
+$(document).ready(function() {
+    var count = 0;
+    $('#newButton').click(function() {
+        $('#testArea').append('<div class="resizable sequenceWidget ui-widget-content" id="resizable' + count +
+                '"><h4 class="ui-widget-header">Resizable</h4><div class="row-fluid"><div class="span6">left</div><div class="span6">right</div></div></div>'
+                );
+        $('#resizable' + count).resizable({
+            maxHeight: 250,
+            maxWidth: 350,
+            minHeight: 150,
+            minWidth: 200
+        });
+        $('#resizable' + count).draggable();
+        $('#resizable' + count).droppable({
+            drop: function(event, ui) {
+                alert("aligning sequence "+$(this).attr("id")+" and "+ui.draggable.attr("id"));
+            },
+            accept: ".sequenceWidget"
+        });
+        count++;
+    });
+});
